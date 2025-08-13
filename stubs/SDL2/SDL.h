2666056e4f070bf8bb8bfe0399e32ba99be19648
Alexander Zheleznov <zheleznov.a.g@gmail.com>
2025-08-13 13:08:14 +0700

    Implement SDL mouse

--- stubs/SDL2/SDL.h
+++ stubs/SDL2/SDL.h
@@ -10,6 +10,7 @@ typedef struct SDL_Renderer {} SDL_Renderer;
 typedef struct SDL_Texture {} SDL_Texture;
 
 typedef uint32_t Uint32;
+typedef uint8_t Uint8;
 typedef int SDL_Keycode;
 typedef struct {
     int type;
@@ -22,6 +23,8 @@ typedef struct {
 #define SDL_TEXTUREACCESS_STREAMING 0
 #define SDL_WINDOWPOS_CENTERED 0
 #define SDL_KEYDOWN 0
+#define SDL_TRUE 1
+#define SDL_FALSE 0
 
 static inline int SDL_Init(Uint32 flags) { (void)flags; return 0; }
 static inline SDL_Window* SDL_CreateWindow(const char*, int, int, int, int, Uint32) { return 0; }
@@ -34,6 +37,9 @@ static inline void SDL_RenderPresent(SDL_Renderer*) {}
 static inline int SDL_PollEvent(SDL_Event*) { return 0; }
 static inline void SDL_Delay(Uint32) {}
 static inline Uint32 SDL_GetTicks(void) { return 0; }
+static inline Uint32 SDL_GetMouseState(int* x, int* y) { if(x) *x = 0; if(y) *y = 0; return 0; }
+static inline int SDL_ShowCursor(int toggle) { return toggle; }
+static inline int SDL_WarpMouseGlobal(int x, int y) { (void)x; (void)y; return 0; }
 
 #ifdef __cplusplus
 }
